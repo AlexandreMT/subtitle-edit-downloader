bfaaba6ffad127ba889fc393946e622cedac0609
Alexandre Moraes Matos <alexandremt03@gmail.com>
2019-08-26 19:11:34 -0300

    readline before unzip

--- src/unzipper.js
+++ src/unzipper.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 const ora = require('ora');
-const unzipper = require('unzipper');
 const currentPath = process.cwd();
+const unzipper = require('unzipper');
+const readlineSync = require('readline-sync');
 
 let spinner;
 
 _unzipper = (selectedAsset) => {
-  spinner = ora('Unzipping...').start();
-  fs.createReadStream(`${currentPath}/${selectedAsset}`)
-    .pipe(unzipper.Extract({ path: `${currentPath}` })
-    .on('close', () => {
-      spinner.succeed();
-    }));
+  if (readlineSync.keyInYN('Unzip file?')) {
+    spinner = ora('Unzipping...').start();
+    fs.createReadStream(`${currentPath}/${selectedAsset}`)
+      .pipe(unzipper.Extract({ path: `${currentPath}` })
+      .on('close', () => {
+        spinner.succeed();
+      }));
+  }
 }
 
 module.exports = {
