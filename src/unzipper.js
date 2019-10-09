26e4a2c10a45bdf8097a4254f77985df4400ef2e
Alexandre Moraes Matos <alexandremt03@gmail.com>
2019-10-09 11:17:18 -0300

    Improvement to delete dowloaded .zip

--- src/unzipper.js
+++ src/unzipper.js
@@ -7,13 +7,31 @@ const readlineSync = require('readline-sync');
 let spinner;
 
 _unzipper = (selectedAsset) => {
-  if (readlineSync.keyInYN('Unzip file?')) {
-    spinner = ora('Unzipping...').start();
-    fs.createReadStream(`${currentPath}/${selectedAsset}`)
-      .pipe(unzipper.Extract({ path: `${currentPath}` })
-      .on('close', () => {
-        spinner.succeed();
-      }));
+  const readStream = `${currentPath}/${selectedAsset}`;
+  const unzipFile = readlineSync.keyInYN('Unzip file?');
+  
+  if (!unzipFile) {
+    return removeDownloadedAsset(readStream);
+  }
+
+  spinner = ora('Unzipping...').start();
+  fs.createReadStream(readStream)
+    .pipe(unzipper.Extract({ path: `${currentPath}` })
+    .on('close', () => {
+      spinner.succeed();
+      return removeDownloadedAsset(readStream);
+    }));
+}
+
+removeDownloadedAsset = (readStream) => {
+  if (readlineSync.keyInYN('Delete .zip file?')) {
+    spinner = ora('Working...').start();
+    try {
+      fs.unlinkSync(readStream);
+      spinner.succeed();
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
